5799ab98d7dac3ec8bf03e8357944a7d54b54c6b
joao-chass <36828679+joao-chass@users.noreply.github.com>
2021-06-01 15:34:41 -0300

    Update ong.routes.js

--- src/routes/ong.routes.js
+++ src/routes/ong.routes.js
@@ -2,7 +2,7 @@ const router = require('express-promise-router')();
 const ongController = require('../controllers/ong.controller');
 // ==> Definindo as rotas do CRUD - 'Ong':
 // ==> Rota responsável por criar um novo 'Ong': (POST): localhost:3000/api/ongs
-//router.post('/ongs', ongController.createOng);
+router.post('/ongs', ongController.createOng);
 // ==> Rota responsável por atualizar 'Ong' pelo 'Id': (PUT): localhost: 3000/api/ongs/:id
 router.put('/ongs/:id', ongController.updateOngById);
 // ==> Rota responsável por excluir 'Ong' pelo 'Id': (DELETE): localhost:3000/api/ongs/:id

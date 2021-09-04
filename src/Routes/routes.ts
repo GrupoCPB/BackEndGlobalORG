50e02a9364a6bad4594012ead1d1880fde2c5a38
D4rknus <dark@outlook.com>
2021-09-04 17:17:20 -0300

    UPDATED DEPENDENCIES

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import AuthVoluntaryController from '../Controller/AuthVoluntaryController';
 import CreateVoluntaryController from '../Controller/CreateVoluntaryController';
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import Logger from '../modules/log';
 
 const routes = Router();
 
@@ -15,6 +16,11 @@ routes.post('/testeAuth', ensureAuthenticated, (req, res) => {
   const { teste } = req.body;
 
   res.json(teste);
+});
+routes.get('/logger', (_, res) => {
+  Logger.error('ISSO É UM ERRO');
+
+  res.send('HELLO WORLD');
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as dotenv from 'dotenv';
+import promBundle from 'express-prom-bundle';
+import helmet from 'helmet';
 import routes from './Routes/routes';
 import swaggerFile from './swagger.json';
 
 import './database';
 
+const metricsMiddleware = promBundle({ includeMethod: true });
+
 dotenv.config();
 
 const app = express();
+app.use(helmet());
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use(metricsMiddleware);
 app.use(routes);
 
 const port = process.env.PORT;

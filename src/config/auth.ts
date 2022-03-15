85c2a41efd36e85148ff53057875868d0bc09a68
Diener Dornelas <dienezim@hotmail.com>
2022-03-15 00:14:43 -0300

    remove files

--- src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: '0ceda2ec001e6ae1bdbd51c584e5ed48',
-    expiresIn: '1d',
-  },
-};
--- src/repositories/VoluntariesRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import Voluntary from '../models/Voluntary';
-
-@EntityRepository(Voluntary)
-export default class VoluntariesRepository extends Repository<Voluntary> {
-  findByEmail(email: string) {
-    return this.findOne({ where: { email } });
-  }
-}

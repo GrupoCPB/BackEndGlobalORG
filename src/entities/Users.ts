28f632faf492ae54ae9180b1ac248d414bae670c
Diener Dornelas <dienezim@hotmail.com>
2022-03-15 00:31:30 -0300

    update entity User

---
+++ src/entities/Users.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @CreateDateColumn()
+  created_at?: Date;
+
+  @CreateDateColumn()
+  updated_at?: Date;
+}

0bc5a30b40a3d40208c3adb4052b13a710b89446
Diener Dornelas <dienezim@hotmail.com>
2022-03-16 14:41:01 -0300

    update config column updated_at

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -3,6 +3,7 @@ import {
   Column,
   CreateDateColumn,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('users')
@@ -25,6 +26,6 @@ export class User {
   @CreateDateColumn()
     created_at?: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
     updated_at?: Date;
 }

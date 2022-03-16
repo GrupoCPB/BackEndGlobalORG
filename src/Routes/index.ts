491c8dbaeedf37466d74bcda7247e9053ffe96ea
Diener Dornelas <dienezim@hotmail.com>
2022-03-16 14:15:33 -0300

    insert route for change password

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { createUserController } from '../useCases/createUser';
-import { loginUserController } from '../useCases/loginUser';
+import { ensureAuthenticated } from '@/middleware/ensureAuthenticated';
+import { createUserController } from '../useCases/user/createUser';
+import { loginUserController } from '../useCases/user/loginUser';
+import { changePasswordController } from '@/useCases/user/changePassword';
 
 const router = Router();
 
 router.get('/', (_, res) => res.json('Base Url'));
 router.post('/api/users', (req, res) => createUserController.handle(req, res));
 router.post('/api/users/auth', (req, res) => loginUserController.handle(req, res));
+router.put('/api/users/change-password', ensureAuthenticated, (req, res) => changePasswordController.handle(req, res));
 
 export { router };
